73eeaba64ee67ce99d9a354b8331bfca090a2626
ins0portable <slim.hustla.420@gmail.com>
2023-04-07 13:43:58 -0600

    Se crea la primera prueba para el Programa1.

---
+++ src/pruebas/pruebaPrograma1.java
@@ -0,0 +1,11 @@
+package pruebas;
+import programas.Programa1;
+
+public class pruebaPrograma1 {
+    public static void main(String[] args) {
+        Programa1 prueba1 = new Programa1();
+        String salida = prueba1.operadorDeClase("D:\\INSOPORTABLE\\Java\\ProcesoDeSofwarePersonal\\src\\pruebas\\prueba1.java");
+
+        System.out.println(salida);
+    }
+}
