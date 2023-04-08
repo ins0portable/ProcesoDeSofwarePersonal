61b30a0d061f4c4690fbbb4a55533dc48a8bc9cc
ins0portable <slim.hustla.420@gmail.com>
2023-04-07 20:43:30 -0600

    Se agrega la clase de prueba para corroborar el correcto funcionamiento del Programa1.

--- src/pruebas/pruebaPrograma1.java
+++ src/pruebas/pruebaPrograma1.java
@@ -4,8 +4,11 @@ import programas.Programa1;
 public class pruebaPrograma1 {
     public static void main(String[] args) {
         Programa1 prueba1 = new Programa1();
-        String salida = prueba1.operadorDeClase("D:\\INSOPORTABLE\\Java\\ProcesoDeSofwarePersonal\\src\\pruebas\\prueba1.java");
-
+        String salida = prueba1.operadorDeClase("D:\\INSOPORTABLE\\Java\\ProcesoDeSofwarePersonal\\src\\pruebas\\programa1_prueba1.java");
         System.out.println(salida);
+
+        Programa1 prueba2 = new Programa1();
+        String salida2 = prueba2.operadorDeClase("D:\\INSOPORTABLE\\Java\\ProcesoDeSofwarePersonal\\src\\pruebas\\programa1_prueba2.java");
+        System.out.println(salida2);
     }
 }
